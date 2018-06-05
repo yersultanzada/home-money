f2fdf71e6f65789039e6c9276e204e517518eca8
yersultanzada <yersultan.zada@gmail.com>
2018-06-05 17:32:21 +0600

    ...

--- src/app/system/records-page/add-category/add-category.component.ts
+++ src/app/system/records-page/add-category/add-category.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, Output} from '@angular/core';
 import {NgForm } from '@angular/forms';
 import {CategoriesService} from '../../shared/services/categories.service';
 import {Category} from '../../shared/models/category.model';
@@ -8,23 +8,25 @@ import {Category} from '../../shared/models/category.model';
   templateUrl: './add-category.component.html',
   styleUrls: ['./add-category.component.scss']
 })
-export class AddCategoryComponent implements OnInit {
+export class AddCategoryComponent {
+
+  @Output() onCategoryAdd = new EventEmitter<Category>();
 
   constructor(private categoriesService: CategoriesService) { }
 
-  ngOnInit() {
-  }
 
 
   onSubmit(form: NgForm) {
-    let {name, capacity} = form.value;
-    if (capacity < 0) {capacity *= -1;}
+    let { name, capacity } = form.value;
+    if (capacity < 0) { capacity *= -1; }
 
     const category = new Category(name, capacity);
 
     this.categoriesService.addCategory(category)
       .subscribe((category: Category) => {
-        console.log(category);
+        form.reset();
+        form.form.patchValue({capacity: 1});
+        this.onCategoryAdd.emit(category);
       });
   }
 }
